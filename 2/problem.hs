398f41543e8e5f37adb0e7adef3a531b60e1530c
YellowForLight <94635645+YellowForLight@users.noreply.github.com>
2023-12-02 06:36:03 +0000

    Day 2

---
+++ 2/problem.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE ViewPatterns #-}
+import Data.List
+import Data.Bifunctor
+import Data.Char
+import Control.Monad
+import Control.Applicative
+import System.IO
+
+parse :: Integral a => Read a => String -> ZipList a
+parse = fmap maximum . traverse (sumFold . fmap (join process) . split ',') . split ';'
+    where
+        split :: Char -> String -> [String]
+        split _ [] = []
+        split c s = let (st, en) = break (==c) s
+                    in st : split c (drop 1 en)
+        readStr :: Read a => String -> a
+        readStr = read . filter (isDigit)
+        process :: Read a => Integral a => String -> String -> ZipList a
+        process (isSuffixOf "red" -> True) s = ZipList [readStr s,0,0]
+        process (isSuffixOf "green" -> True) s = ZipList [0, readStr s,0]
+        process (isSuffixOf "blue" -> True) s = ZipList [0, 0, readStr s]
+        sumFold :: Integral a => [ZipList a] -> ZipList a
+        sumFold = foldl (\a b -> pure (+) <*> a <*> b) $ pure (0)
+
+
+ids :: Integral a => Read a => String -> (a, Bool)
+ids  = bimap getId (all id . getZipList . (pure (>=) <*> ZipList [12, 13, 14] <*>) . parse . drop 1) . break (==':')
+    where
+        getId :: Read a => Integral a => String -> a
+        getId (stripPrefix "Game " -> Just s) = read s
+
+calc :: Integral a => Read a => String -> a
+calc = sum . fmap fst . filter snd . fmap ids . lines
+
+main = do
+    text <- hGetContents stdin
+    hPrint stdout $ calc text
