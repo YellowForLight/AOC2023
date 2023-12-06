94ada36e333376b784c9c505d727d759133cd81e
YellowForLight <94635645+YellowForLight@users.noreply.github.com>
2023-12-06 05:42:53 +0000

    Day 6

---
+++ 6/problem2.hs
@@ -0,0 +1,19 @@
+import Data.List
+import Data.Tuple
+import System.IO
+
+process :: Integral a => (a, a) -> a
+process = uncurry inner
+    where
+        inner :: Integral a => a -> a -> a
+        inner b c 
+            | b `rem` 2 == 0 = -1 + 2 * ceiling (sqrt (fromIntegral $ b^2 - 4*c) / 2)
+            | b `rem` 2 == 1 = -2 + 2 * ceiling (0.5 + sqrt (fromIntegral $ b^2 - 4*c) / 2)
+
+calc :: Integral a => Read a => String -> a
+calc = process . toTup . fmap (read . concat . drop 1 . words) . lines
+    where toTup (a:b:[]) = (a, b)
+
+main = do
+    text <- hGetContents stdin
+    hPrint stdout $ calc text
