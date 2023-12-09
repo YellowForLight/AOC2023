9ce8c04d42842d710e781578b6c24835c8750111
YellowForLight <94635645+YellowForLight@users.noreply.github.com>
2023-12-09 05:23:20 +0000

    Day 9

---
+++ 9/problem2.hs
@@ -0,0 +1,17 @@
+import Data.List
+import System.IO
+
+process :: Integral a => [a] -> a
+process ns 
+    | all (==0) ns = 0
+    | otherwise = head ns - process (unfoldr diffs ns)
+    where
+        diffs (a : ns@(b : _)) = Just ((b-a), ns)
+        diffs (_ : []) = Nothing
+
+calc :: Integral a => Read a => String -> a
+calc = sum . fmap (process . fmap read . words) . lines
+
+main = do
+    text <- hGetContents stdin
+    hPrint stdout $ calc text
