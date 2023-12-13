76e84403731c367f62b34d5825527420f8cc50b9
YellowForLight <94635645+YellowForLight@users.noreply.github.com>
2023-12-13 05:47:06 +0000

    Day 13

---
+++ 13/problem.hs
@@ -0,0 +1,38 @@
+import Data.List
+import Data.Bifunctor
+import System.IO
+
+data Zipper a = Zipper [a] [a]
+forward :: Zipper a -> Maybe (Zipper a)
+forward (Zipper _ []) = Nothing
+forward (Zipper _ (r:[])) = Nothing
+forward (Zipper ls (r : rs)) = Just $ Zipper (r : ls) rs
+numAbove :: Integral a => Zipper b -> a
+numAbove (Zipper ls _) = genericLength ls
+isReflect :: Eq b => Zipper b -> Bool
+isReflect (Zipper [] _) = False
+isReflect (Zipper _ []) = False
+isReflect (Zipper ls rs) = all id $ zipWith (==) ls rs
+zipify :: [a] -> Zipper a
+zipify = Zipper []
+reset :: Zipper a -> [a]
+reset (Zipper ls rs) = reverse ls ++ rs
+
+process :: Integral a => Eq b => Zipper [b] -> a
+process zipper = inner (forward zipper)
+    where
+        inner (Just z)
+            | isReflect z = 100 * numAbove z
+            | otherwise = process z
+        inner Nothing = flip quot 100 $ process (zipify $ transpose $ reset zipper)
+
+calc :: Integral a => String -> a
+calc = sum . fmap (process . zipify) . unfoldr split . lines
+    where
+        split :: [String] -> Maybe ([String], [String])
+        split [] = Nothing
+        split s = Just $ bimap id (drop 1) (break (=="") s) 
+
+main = do
+    text <- hGetContents stdin
+    print $ calc text
